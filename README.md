321f386656df47b6f31bdf795f0cc339feb27dd1
halloleo <git@halloleo.dot.hailmail.dot.net>
2017-11-01 18:50:34 +1100

    Cleanup and more details

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
-`arghex`: Extensions for `argh`, the "Natural CLI"
+ArghEx: Extensions for Argh, the "Natural CLI"
 =========
 
-This module contains some extensions to the Python CLI package `argh`. These extensions proved useful to me.
+This module contains some extensions to the Python CLI package `[argh][]`. Currently the only extension provided is a command-line parser which collects unknown arguments in a list.
+
 
 ### Installation
 
 Just use `arghex.py` in your Python project.
+
+### Parser With Unknown Arguments
+
+`ArghParserWithUnknownArgs` is a class derived from `[ArghParser][]`
+and it collects unknown arguments in a list for further
+processing. 
+
+When you instantiate `ArghParserWithUnknownArgs` you give it the name of the variable holding the list of unknown arguments. See the following code snippet:
+
+```
+def main(my_arg=False, other_args=[]):
+    # use my_arg here
+    # and pass other_args through
+
+if __name__ == '__main__':
+    parser = arghex.ArghParserWithUnknownArgs('other_args')
+```
+
+### Licensing
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+[argh]: https://github.com/neithere/argh
+[ArghParser]: http://argh.readthedocs.io/en/latest/reference.html#argh.helpers.ArghParser
--- arghex.py
+++ arghex.py
@@ -4,25 +4,21 @@
 #
 #  This file is not part of Argh.
 #
-#  arghex is free software under terms of the GNU Lesser
-#  General Public License version 3 (LGPLv3)
+#  ArghEx is free software under terms of the
+#  GNU General Public License version 3
 #
 """
 Argh Extensions
 ~~~~~~~
 """
 
-__version__ = '0.2'
-
 import argparse
 import argh
 from argh.dispatching import ArghNamespace
-from argh.constants import (ATTR_ALIASES, ATTR_ARGS, ATTR_NAME,
-                            ATTR_WRAPPED_EXCEPTIONS,
-                            ATTR_WRAPPED_EXCEPTIONS_PROCESSOR,
-                            ATTR_EXPECTS_NAMESPACE_OBJECT)
+from argh.constants import (ATTR_ARGS)
 from argh.compat import OrderedDict
 
+__version__ = '0.3'
 __all__ = ['ArghParserWithUnknownArgs']
 
 
@@ -31,10 +27,10 @@ class ArghParserWithUnknownArgs(argh.ArghParser):
     A subclass of :class:`ArghParser` with support for
     unknown arguments
     """
-    unknown_args_var = ''
+    unknown_args_varname = ''
 
-    def __init__(self, unknown_args_var, *args, **kwargs):
-        self.unknown_args_var = unknown_args_var
+    def __init__(self, unknown_args_varname, *args, **kwargs):
+        self.unknown_args_varname = unknown_args_varname
         super(ArghParserWithUnknownArgs, self).__init__(*args, **kwargs)
 
     def parse_args(self, args=None, namespace=None):
@@ -49,35 +45,36 @@ class ArghParserWithUnknownArgs(argh.ArghParser):
             required for functions to be properly used as commands.
         """
         namespace = namespace or ArghNamespace()
-        (namespace_obj, unknown_args) = super(ArghParserWithUnknownArgs, self).parse_known_args(args=args, namespace=namespace)
-        namespace_obj.__dict__[self.unknown_args_var] = unknown_args
+        (namespace_obj, unknown_args) = \
+            super(ArghParserWithUnknownArgs, self).parse_known_args(args=args, namespace=namespace)
+        namespace_obj.__dict__[self.unknown_args_varname] = unknown_args
         return namespace_obj
 
 
-def set_default_command(parser, function):
+def set_default_command(parser, func):
     """
     Replacement for :meth:`ArgParser.set_default_command`.
     It does add the parser's arg variable for the unknown args
-    as suppressed argument to the function
+    as suppressed argument to the func
     """
+
     def _get_option_from_var(var):
         return '--{0}'.format(var.replace('_', '-'))
 
-    unknown_args_var = getattr(parser, 'unknown_args_var', None)
+    unknown_args_var = getattr(parser, 'unknown_args_varname', None)
     if unknown_args_var is not None:
         unknown_args_suppress_dict = \
             {'help': argparse.SUPPRESS,
              'option_strings': (_get_option_from_var(unknown_args_var),)}
-        # prepare for checking whether unknown_args is in function signature
-        inferred_args = list(argh.assembling._get_args_from_signature(function))
+        # prepare for checking whether unknown_args is in func signature
+        inferred_args = list(argh.assembling._get_args_from_signature(func))
         dests = OrderedDict()
         for argspec in inferred_args:
             dest = argh.assembling._get_parser_param_kwargs(parser, argspec)['dest']
             dests[dest] = argspec
         if argh.assembling._get_dest(parser, unknown_args_suppress_dict) in dests:
-            declared_args = getattr(function, ATTR_ARGS, [])
+            declared_args = getattr(func, ATTR_ARGS, [])
             declared_args.insert(0, unknown_args_suppress_dict)
-            setattr(function, ATTR_ARGS, declared_args)
+            setattr(func, ATTR_ARGS, declared_args)
 
-    argh.set_default_command(parser, function)
-
+    argh.set_default_command(parser, func)
